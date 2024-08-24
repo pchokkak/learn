f701fb7c7452e727666c1c301c2b4314b26cf17e
pchokkak <pchokkak@gmail.com>
2024-08-25 02:57:15 +0530

    new

--- find_pair.py
+++ find_pair.py
@@ -1,8 +1,17 @@
 def find_pair(A,sum):
     for i in range(len(A)-1):
-        print i
-        
+        print(i)
+        for j in range(i+1, len(A)):
+            if A[i]+A[j] == sum:
+                print("pair found at index{0} and {1} ie".format(i,j))
+                print(A[i]+A[j])
+                
+    print("pair not found")
 
-A = [5,3,9,7,1,4,6]
-sum = 10
-find_pair(A,sum)
+if __name__ == '__main__':
+
+    A = [5,3,9,7,1,4,6]
+    sum = 10
+
+    find_pair(A,sum)
+
